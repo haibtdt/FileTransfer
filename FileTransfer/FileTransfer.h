e2e52a1d9a65b076212879dda838564425d4360d
Hai Bui <haibtdt@gmail.com>
2015-10-09 20:40:27 +0700

    [ADD] a new ios project

---
+++ FileTransfer/FileTransfer.h
@@ -0,0 +1,19 @@
+//
+//  FileTransfer.h
+//  FileTransfer
+//
+//  Created by Hai Bui on 10/9/15.
+//  Copyright © 2015 Hai Bui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileTransfer.
+FOUNDATION_EXPORT double FileTransferVersionNumber;
+
+//! Project version string for FileTransfer.
+FOUNDATION_EXPORT const unsigned char FileTransferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileTransfer/PublicHeader.h>
+
+
